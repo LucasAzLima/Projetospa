1fb8a66aa5d0316bf4a77235ba900832397e4100
Lucas de Azevedo Lima <Lucas.a.lima13@outlook.com>
2018-05-08 22:39:01 -0300

    Add files via upload
    primeiro projeto

---
+++ projeto-pa/retangulo.cpp
@@ -0,0 +1,10 @@
+#include "retangulo.h"
+
+Retangulo::Retangulo(float x, float y, float largura, float altura){
+    vertices[0].setXY(x,y);
+    vertices[1].setXY(x,y-altura);
+    vertices[2].setXY(x+largura,y-altura);
+    vertices[3].setXY(x+largura,y);
+
+    nv = 4;
+}
